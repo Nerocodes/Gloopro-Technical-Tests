16769d49f5cc24f6b348fa25cc756470f88aca9e
danoseun <ayodejisomefun@yahoo.com>
2021-04-12 17:07:18 +0100

    moving coding assessment attempt to github

--- javascript/question1.js
+++ javascript/question1.js
@@ -1,6 +1,11 @@
 // Complete the saveThePrisoner function below.
 function saveThePrisoner(n, m, s) {
-  return null;
+  return (((m - 1) + s) % n) || n;
 }
 
+/**
+ * n=no of prisoners
+ * m=no of sweets
+ * s=chair no to begin passing the sweets
+ */
 module.exports = saveThePrisoner
--- javascript/question2.js
+++ javascript/question2.js
@@ -1,6 +1,29 @@
 // Complete the beautifulDays function below.
 function beautifulDays(i, j, k) {
-  return null;
+  const numberArray=[];
+  for(let n=i; n<=j; n++){
+    numberArray.push(n);
+  }
+  
+  const resultArray = [];
+  for(let a=0; a < numberArray.length; a++){
+    if(Math.abs(numberArray[a] - splitAndReverseNumber(numberArray[a])) % k === 0){
+      resultArray.push(numberArray[a])
+    }
+  }
+  return resultArray.length;
+}
+
+/**
+ * Helper function
+ * takes in an integer
+ * converts to string and reverses it
+ * then converts back to integer
+ * @param {integer} n 
+ * @returns integer
+ */
+function splitAndReverseNumber(n){
+  return Number(n.toString().split('').reverse().join(''));
 }
 
 module.exports = beautifulDays
